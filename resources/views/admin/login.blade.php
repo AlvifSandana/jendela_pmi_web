34747c6508516c566a2702994214611fc235ae44
Alvif Sandana Mahardika <asm240800@gmail.com>
2021-07-07 12:23:33 +0700

    fix color for sidebar, login, and header item menu

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -9,7 +9,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>Sufee Admin - HTML5 Admin Template</title>
+  <title>Jendela PMI - Login Portal</title>
   <meta name="description" content="Sufee Admin - HTML5 Admin Template">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -18,9 +18,17 @@
 
   @include('layouts.css')
   <style>
-      .bg-merah{
-          background-color: #bd2323
-      }
+    .bg-merah {
+      background-color: #bd2323
+    }
+    .form-control{
+        border-color: #bd2323
+    }
+    .form-control:focus {
+      border-color: #FF0000;
+      box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.075), 0 0 8px rgba(255, 0, 0, 0.6);
+    }
+
   </style>
 
   <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800' rel='stylesheet' type='text/css'>
@@ -34,7 +42,8 @@
         <div class="login-form">
           <div class="login-logo mt-2 mb-4">
             <a href="index.html">
-              <img class="align-content" src="{{ asset('images/logo-pmi-bwi.png')}}" alt="logopmibwi" style="width: 50%;">
+              <img class="align-content" src="{{ asset('images/logo-pmi-bwi.png') }}" alt="logopmibwi"
+                style="width: 50%;">
             </a>
           </div>
           <form class=" text-center">
@@ -44,7 +53,8 @@
             <div class="form-group">
               <input type="password" class="form-control" placeholder="Password">
             </div>
-            <button type="submit" class="btn bg-merah text-white  m-b-30 m-t-30" style="width: 80px !important;">Masuk</button>
+            <button type="submit" class="btn bg-merah text-white shadow m-b-30 m-t-30 mb-4"
+              style="width: 80px !important;">Masuk</button>
           </form>
         </div>
       </div>
@@ -52,10 +62,7 @@
   </div>
 
 
-  <script src="vendors/jquery/dist/jquery.min.js"></script>
-  <script src="vendors/popper.js/dist/umd/popper.min.js"></script>
-  <script src="vendors/bootstrap/dist/js/bootstrap.min.js"></script>
-  <script src="assets/js/main.js"></script>
+  @include('layouts.js')
 
 
 </body>
