c5e98d4a4b15ee53c6b6eb2c4f8438c06ab1a8f9
AlvifSandana <asm240800@gmail.com>
2021-07-21 21:53:16 +0700

    add stok darah seeder

---
+++ database/seeds/StokDarahSeeder.php
@@ -0,0 +1,98 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class StokDarahSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('stok_darah')->insert([
+            [
+                'produk'        => 'WB',
+                'jenis_darah'   => 'A',
+                'jumlah'        => 8
+            ],
+            [
+                'produk'        => 'WB',
+                'jenis_darah'   => 'B',
+                'jumlah'        => 3
+            ],
+            [
+                'produk'        => 'WB',
+                'jenis_darah'   => 'AB',
+                'jumlah'        => 5
+            ],
+            [
+                'produk'        => 'WB',
+                'jenis_darah'   => 'O',
+                'jumlah'        => 4
+            ],
+            [
+                'produk'        => 'PRC',
+                'jenis_darah'   => 'A',
+                'jumlah'        => 110
+            ],
+            [
+                'produk'        => 'PRC',
+                'jenis_darah'   => 'B',
+                'jumlah'        => 45
+            ],
+            [
+                'produk'        => 'PRC',
+                'jenis_darah'   => 'AB',
+                'jumlah'        => 33
+            ],
+            [
+                'produk'        => 'PRC',
+                'jenis_darah'   => 'O',
+                'jumlah'        => 55
+            ],
+            [
+                'produk'        => 'TC',
+                'jenis_darah'   => 'A',
+                'jumlah'        => 33
+            ],
+            [
+                'produk'        => 'TC',
+                'jenis_darah'   => 'B',
+                'jumlah'        => 66
+            ],
+            [
+                'produk'        => 'TC',
+                'jenis_darah'   => 'AB',
+                'jumlah'        => 99
+            ],
+            [
+                'produk'        => 'TC',
+                'jenis_darah'   => 'O',
+                'jumlah'        => 21
+            ],
+            [
+                'produk'        => 'FFP',
+                'jenis_darah'   => 'A',
+                'jumlah'        => 89
+            ],
+            [
+                'produk'        => 'FFP',
+                'jenis_darah'   => 'B',
+                'jumlah'        => 31
+            ],
+            [
+                'produk'        => 'FFP',
+                'jenis_darah'   => 'AB',
+                'jumlah'        => 5
+            ],
+            [
+                'produk'        => 'FFP',
+                'jenis_darah'   => 'O',
+                'jumlah'        => 9
+            ],
+        ]);
+    }
+}
